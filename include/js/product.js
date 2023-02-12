aaabf298c6fb3304e6544f14e89b251782040b86
ftheriau01 <ftheriau01@gmail.com>
2023-02-12 12:04:34 -0600

    v20

--- include/js/product.js
+++ include/js/product.js
@@ -1,13 +1,40 @@
 
 $('#cardImg').mouseenter(function() {
-    $('#hide').slideDown(400 ,'easeOutSine');
+    $('#hide').slideDown('fast' ,'easeOutSine');
  });
 $('#hide').mouseleave(function(){
-    $(this).slideUp(400 ,'easeInSine');
+    $(this).slideUp('fast' ,'easeInSine');
 });
 $('#cardImg1').mouseenter(function() {
-    $('#hide1').slideDown(400 ,'easeOutSine');
+    $('#hide1').slideDown('fast' ,'easeOutSine');
  });
 $('#hide1').mouseleave(function(){
-    $(this).slideUp(400,'easeInSine');
+    $(this).slideUp('fast','easeInSine');
 });
+$('#cardImg2').mouseenter(function() {
+    $('#hide2').slideDown('fast' ,'easeOutSine');
+ });
+$('#hide2').mouseleave(function(){
+    $(this).slideUp('fast','easeInSine');
+});
+$('#cardImg3').mouseenter(function() {
+    $('#hide3').slideDown('fast' ,'easeOutSine');
+ });
+$('#hide3').mouseleave(function(){
+    $(this).slideUp('fast','easeInSine');
+});
+$('#cardImg4').mouseenter(function() {
+    $('#hide4').slideDown('fast' ,'easeOutSine');
+ });
+$('#hide4').mouseleave(function(){
+    $(this).slideUp('fast','easeInSine');
+});
+$('#cardImg5').mouseenter(function() {
+    $('#hide5').slideDown('fast' ,'easeOutSine');
+ });
+$('#hide5').mouseleave(function(){
+    $(this).slideUp('fast','easeInSine');
+});
+$( function() {
+    $( "#accordion" ).accordion();
+  } );
