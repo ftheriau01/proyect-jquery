2faa7eca1c9f74924d0fca71f16f26594d44a645
ftheriau01 <ftheriau01@gmail.com>
2023-02-11 22:41:07 -0600

    v17

--- include/js/product.js
+++ include/js/product.js
@@ -1,6 +1,13 @@
-$('#img').mouseenter(function() {
-    $('#hide').slideDown(600,'easeInQuad');
+
+$('#cardImg').mouseenter(function() {
+    $('#hide').slideDown(400 ,'easeOutSine');
  });
 $('#hide').mouseleave(function(){
-    $(this).slideUp(900 ,'easeInOutBack');
+    $(this).slideUp(400 ,'easeInSine');
+});
+$('#cardImg1').mouseenter(function() {
+    $('#hide1').slideDown(400 ,'easeOutSine');
+ });
+$('#hide1').mouseleave(function(){
+    $(this).slideUp(400,'easeInSine');
 });
